64a174887601a480be44ad7f71006aeb909a4b90
www-data <www-data@xfer.(none)>
2011-03-09 15:38:20 -0800

    Convert to HTML5. Try to fix paths. Not there yet.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ob_start();
 
 $uploadedfile = $_FILES['uploadedfile'];
 
-$share_path = '/var/www/share/';
+$share_path = 'share/';
 
 if ($uploadedfile) {
   $target_path = $share_path.basename(str_replace(' ','_',$uploadedfile['name']));
@@ -22,31 +22,27 @@ function hsize($size) {
 function link_file($file) {
   global $share_path;
   $text = basename($file);
-  $href = '../share/'.$text;
-  $download = '../download/'.$text;
+  $href = 'share/'.$text;
+  $download = 'download/'.$text;
   $size = filesize($share_path.$file);
   $link = '<tr><td><a href="'.$href.'" title="Open file">'.$text.'</a></td><td style="text-align:center;width:20px"><a href="'.$download
     .'" title="Save file">&darr;</a></td><td style="text-align:right">'.hsize($size).'</td></tr>';
   return $link;
 }
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
+?><!doctype html>
     <title>ITG uploader</title>
     <link rel="stylesheet" href="style.css?<?= time() ?>" type="text/css" />
-  </head>
-  <body>
     <h1>ITG uploader</h1>
     <?= $error; ?>
     <? if ( $success ) { ?>
-    <p><strong><?= $uploadedfile['name'] ?></strong>: <?= $uploadedfile['size'] ?> bytes of type <?= $uploadedfile['type'] ?> uploaded</p>
+    <p><strong><?= $uploadedfile['name'] ?></strong>: <?= $uploadedfile['size'] ?> bytes of type <?= $uploadedfile['type'] ?> uploaded
     <? } ?>
     <form enctype="multipart/form-data" action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
       <fieldset>
         <legend>Upload a file</legend>
-        <p class="darkred">Caution: If you upload a file with the same name, your new file will overwrite the old one!</p>
-        <input name="uploadedfile" type="file" /> <input type="submit" value="Upload" />
+        <p class="darkred">Caution: If you upload a file with the same name, your new file will overwrite the old one!
+        <p><input name="uploadedfile" type="file" /> <input type="submit" value="Upload" />
       </fieldset>
     </form>
 
@@ -54,7 +50,7 @@ function link_file($file) {
     <table>
 
 <?
-if ($handle = opendir('/var/www/share')) {
+if ($handle = opendir($share_path)) {
 
   while (false !== ($file = readdir($handle))) $files[] = $file;
 
@@ -69,3 +65,4 @@ if ($handle = opendir('/var/www/share')) {
     </table>
   </body>
 </html>
+
